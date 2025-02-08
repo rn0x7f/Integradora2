cb032046b05d8c491b45ab184d701d994a2c04c4
rn0x7f <ampheriz0@gmail.com>
2025-02-08 07:11:57 -0600

    jkjikmnjkm

--- tests/nearest_central.cpp
+++ tests/nearest_central.cpp
@@ -2,6 +2,8 @@
 // tests/nearest_central.cpp
 #include "../include/NearestCentralLib.h"
 #include <gtest/gtest.h>
+#include <sstream>
+
 
 TEST(NearestCentralTest, ClosestPoint) {
     std::vector<Point> centrales = {
@@ -15,4 +17,38 @@ TEST(NearestCentralTest, ClosestPoint) {
     Point closest = find_nearest_central(centrales, query);
     EXPECT_EQ(closest.x, 450);
     EXPECT_EQ(closest.y, 150);
+}
+
+TEST(NearestCentralTest, ParsePoint) {
+    std::string input = "(400,300)";
+    Point result = parse_point(input);
+    EXPECT_EQ(result.x, 400);
+    EXPECT_EQ(result.y, 300);
+}
+
+TEST(NearestCentralTest, ReadCentrales) {
+    std::istringstream input_stream("(400,300)\n(100,200)\n(300,400)\n");
+    std::cin.rdbuf(input_stream.rdbuf());  // Redirigir cin a input_stream
+
+    std::vector<Point> centrales;
+    Point query;
+    read_centrales(3, centrales, query);
+
+    // Verificar que las centrales y el punto de consulta se leyeron correctamente
+    EXPECT_EQ(centrales.size(), 3);
+    EXPECT_EQ(centrales[0].x, 400);
+    EXPECT_EQ(centrales[0].y, 300);
+    EXPECT_EQ(query.x, 300);
+    EXPECT_EQ(query.y, 400);
+}
+
+TEST(NearestCentralTest, PrintNearestCentral) {
+    Point central = {450, 150};
+
+    testing::internal::CaptureStdout();  // Capturar la salida estándar
+    print_nearest_central(central);
+    std::string output = testing::internal::GetCapturedStdout();
+
+    // Verificar que la salida sea la esperada
+    EXPECT_EQ(output, "4.\n(450,150)\n");
 }
